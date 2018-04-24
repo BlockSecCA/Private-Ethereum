8c4d5a63dd6de1513c44e69b220b4705a20c5d4f
BlockSecCarlos <carlos@altoazul.com>
2018-04-24 11:20:42 -0400

    Removed Tree structure

---
+++ _vti_cnf/usera.sh
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|24 Apr 2018 15:19:08 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|Allyourbase\\Carlos
+vti_modifiedby:SR|Allyourbase\\Carlos
+vti_nexttolasttimemodified:TR|24 Apr 2018 13:04:34 -0000
+vti_timecreated:TR|24 Apr 2018 15:19:08 -0000
+vti_cacheddtm:TX|24 Apr 2018 15:19:08 -0000
+vti_filesize:IR|290
+vti_backlinkinfo:VX|
