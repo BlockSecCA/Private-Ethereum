b1bbb24aab5ca0eabf2e7b5bd089d714fb32fe6c
BlockSecCarlos <carlos@altoazul.com>
2018-04-24 11:37:20 -0400

    Broken links fixed
    Bunch of broken links leftover from directory structure (now gone)

--- _vti_cnf/2_deploy_contracts.js
+++ _vti_cnf/2_deploy_contracts.js
@@ -5,6 +5,6 @@ vti_backlinkinfo:VX|hello-world-smart-contract.html
 vti_author:SR|Allyourbase\\Carlos
 vti_modifiedby:SR|Allyourbase\\Carlos
 vti_timecreated:TR|24 Apr 2018 13:04:34 -0000
+vti_nexttolasttimemodified:TR|24 Apr 2018 13:04:34 -0000
 vti_cacheddtm:TX|24 Apr 2018 15:19:52 -0000
 vti_filesize:IR|356
-vti_nexttolasttimemodified:TR|24 Apr 2018 13:04:34 -0000
--- _vti_cnf/usera.sh
+++ _vti_cnf/usera.sh
@@ -7,4 +7,4 @@ vti_nexttolasttimemodified:TR|24 Apr 2018 13:04:34 -0000
 vti_timecreated:TR|24 Apr 2018 15:19:08 -0000
 vti_cacheddtm:TX|24 Apr 2018 15:19:08 -0000
 vti_filesize:IR|290
-vti_backlinkinfo:VX|
+vti_backlinkinfo:VX|index.html
